c25421048441f5561be1b91ff478eb67b7dbab62
AdmiralXy <gameproblem80@gmail.com>
2022-08-30 03:35:48 +0300

    configure database connection and versioning

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO "films" ("title", "release_year", "age_restrictions", "description")
+VALUES ('Inception', 2010, '12+', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.'),
+       ('Interstellar', 2014, '16+', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity`s survival.'),
+       ('The Matrix', 1999, '16+', 'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.'),
+       ('Schindler`s List', 1993, '16+', 'The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.'),
+       ('Catch Me If You Can', 2002, '12+', 'A true story about Frank Abagnale Jr. who, before his 19th birthday, successfully conned millions of dollars worth of checks as a Pan Am pilot, doctor, and legal prosecutor. An FBI agent makes it his mission to put him behind bars. But Frank not only eludes capture, he revels in the pursuit.'),
+       ('Shutter Island', 2009, '18+', 'World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by troubling visions and a mysterious doctor.'),
+       ('Django Unchained', 2012, '18+', 'With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.'),
+       ('Dallas Buyers Club', 2013, '18+', 'Loosely based on the true-life tale of Ron Woodroof, a drug-taking, women-loving, homophobic man who in 1986 was diagnosed with HIV/AIDS and given thirty days to live.'),
+       ('The Man from Earth', 2007, '18+', 'An impromptu goodbye party for Professor John Oldman becomes a mysterious interrogation after the retiring scholar reveals to his colleagues he never ages and has walked the earth for 14,000 years.'),
+       ('Blade Runner 2049', 2017, '18+', 'Thirty years after the events of the first film, a new blade runner, LAPD Officer K, unearths a long-buried secret that has the potential to plunge what`s left of society into chaos. K`s discovery leads him on a quest to find Rick Deckard, a former LAPD blade runner who has been missing for 30 years.'),
+       ('Inglourious Basterds', 2009, '16+', 'In Nazi-occupied France during World War II, a group of Jewish-American soldiers known as "The Basterds" are chosen specifically to spread fear throughout the Third Reich by scalping and brutally killing Nazis. The Basterds, lead by Lt. Aldo Raine soon cross paths with a French-Jewish teenage girl who runs a movie theater in Paris which is targeted by the soldiers.'),
+       ('The Big Short', 2015, '18+', 'In 2006-2007 a group of investors bet against the US mortgage market. In their research, they discover how flawed and corrupt the market is.');
+
+INSERT INTO "halls" ("serial", "seats")
+VALUES (1, 50),
+       (2, 10),
+       (3, 25),
+       (4, 65),
+       (5, 70);
+
+INSERT INTO "sessions" ("hall_id", "film_id", "start_at", "cost")
+VALUES (2, 5, '2022-10-01 10:25:00', 3.5),
+       (1, 4, '2022-10-02 16:10:00', 4),
+       (4, 7, '2022-10-03 22:00:00', 5),
+       (2, 8, '2022-10-06 00:25:00', 8),
+       (2, 1, '2022-10-07 11:40:00', 2.5),
+       (1, 10, '2022-10-13 13:15:00', 12.25),
+       (1, 4, '2022-10-14 18:50:00', 4),
+       (3, 12, '2022-10-23 22:30:00', 10),
+       (4, 2, '2022-10-25 09:30:00', 7);
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS films (
+    id IDENTITY NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    release_year INT NOT NULL,
+    age_restrictions VARCHAR(255) NOT NULL,
+    description CLOB NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS halls (
+    id IDENTITY NOT NULL PRIMARY KEY,
+    serial INT NOT NULL UNIQUE,
+    seats INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    id IDENTITY NOT NULL PRIMARY KEY,
+    hall_id INT NOT NULL,
+    film_id INT NOT NULL,
+    start_at TIMESTAMP NOT NULL,
+    cost DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (hall_id) REFERENCES halls(id),
+    FOREIGN KEY (film_id) REFERENCES films(id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id IDENTITY NOT NULL PRIMARY KEY,
+    film_id INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    message CLOB NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (film_id) REFERENCES films(id)
+);
